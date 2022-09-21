f687a0eb22906d6b14f91743ff3069b567d1467e
IanSantana <42079086+IanSantana@users.noreply.github.com>
2022-09-21 14:00:30 -0300

    Add files via upload

--- ComprasNetFilter.py
+++ ComprasNetFilter.py
@@ -2,6 +2,7 @@
 import datetime, time, os
 from datetime import date
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from datetime import timedelta
 from flashtext import KeywordProcessor
@@ -33,38 +34,38 @@ def busca():
     try:
         today = date.today() + timedelta(days=1) # fazer filtro para remover finais de semana
         dateH = datetime.datetime.strptime(str(today), '%Y-%m-%d').strftime('%d/%m/%Y')
-        calendario = browser.find_element_by_xpath('//*[@id="dt_abertura"]')
+        calendario = browser.find_element(By.XPATH,'//*[@id="dt_abertura"]')
         calendario.send_keys(dateH) #inserir data
-        situacao = browser.find_element_by_xpath('//*[@id="lstSituacao"]/option[text()="Aberto para Proposta"]').click()
-        confirmar = browser.find_element_by_xpath('//*[@id="ok"]').click()
+        situacao = browser.find_element(By.XPATH,'//*[@id="lstSituacao"]/option[text()="Aberto para Proposta"]').click()
+        confirmar = browser.find_element(By.XPATH,'//*[@id="ok"]').click()
         window_before = browser.window_handles[0]
         window_before_title = "principal"
     except Exception:
         lista = open(os.path.abspath('lista.txt'),'w')
         lista.write("O site está com problemas")
-    limite = browser.find_element_by_css_selector('#QtdPregoes')
+    limite = browser.find_element(By.CSS_SELECTOR,'#QtdPregoes')
     aux = limite.text.replace('=', '')
-    totalP = aux.replace('pregões', '')
+    totalP = aux.replace('licitações', '')
     
     while i < (int(totalP) + 2):
         preparaUrlC1 = '/html/body/table/tbody/tr[2]/td/table[2]/tbody/tr[2]/td[2]/table/tbody/tr/td/table/tbody/tr[3]/td/table/tbody/tr[i]/td[j]/a'
         preparaUrlC1 = preparaUrlC1.replace('i', str(i))
         preparaUrlC1 = preparaUrlC1.replace('j', str(j))
-        browser.find_element_by_xpath(preparaUrlC1).click()
+        browser.find_element(By.XPATH,preparaUrlC1).click()
         #time.sleep(1)
         window_after = browser.window_handles[1]
         window_after_title = "detalhes"
         browser.switch_to.window(window_after)
-        buscar = browser.find_element_by_xpath('html')
+        buscar = browser.find_element(By.XPATH,'html')
         if bool(kp.extract_keywords(buscar.text)):
             keywords_found = kp.extract_keywords(buscar.text)
             keywordaux = keywords_found
             browser.switch_to.window(window_before)
-            resultadov1 = browser.find_element_by_xpath(preparaUrlC1)
+            resultadov1 = browser.find_element(By.XPATH,preparaUrlC1)
             preparaUrlC2 = '/html/body/table/tbody/tr[2]/td/table[2]/tbody/tr[2]/td[2]/table/tbody/tr/td/table/tbody/tr[3]/td/table/tbody/tr[i]/td[j]'
             preparaUrlC2 = preparaUrlC2.replace('i', str(i))
             preparaUrlC2 = preparaUrlC2.replace('j', '2')
-            resultadov2 = browser.find_element_by_xpath(preparaUrlC2)
+            resultadov2 = browser.find_element(By.XPATH,preparaUrlC2)
             listaC.append((resultadov1.text, resultadov2.text, keywordaux))
             
         browser.switch_to.window(window_before)
--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import datetime, time, os
 from datetime import date
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
 from datetime import timedelta
 from flashtext import KeywordProcessor
 from os import path
